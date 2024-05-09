dc7c9fec8683b9186a98e48eaedff48517583568
nagisa599 <nagi46460510@icloud.com>
2024-05-09 12:42:33 +0900

    test

--- bff/src/auth/jwt-auth-guard.ts
+++ bff/src/auth/jwt-auth-guard.ts
@@ -6,6 +6,8 @@ import {
 } from '@nestjs/common';
 import * as jwt from 'jsonwebtoken';
 import * as jwksClient from 'jwks-rsa';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Injectable()
 export class AuthGuard implements CanActivate {
@@ -33,17 +35,21 @@ export class AuthGuard implements CanActivate {
 
   private async validateToken(token: string): Promise<any> {
     const decoded: any = jwt.decode(token, { complete: true });
+    console.log('decoded', decoded);
     if (!decoded) throw new UnauthorizedException('Invalid token');
     const kid = decoded.header.kid;
+    console.log('kid', kid);
+    console.log('env', process.env.AUTH0_ISSUER_URL);
     const key = await this.client.getSigningKey(kid);
     const signingKey = key.getPublicKey();
-
+    console.log('signiture', signingKey);
     try {
       jwt.verify(token, signingKey, {
         algorithms: ['RS256'],
         audience: process.env.AUTH0_AUDIENCE,
         issuer: `${process.env.AUTH0_ISSUER_URL}/`,
       });
+      console.log('test');
       console.log(decoded.payload);
       return decoded.payload; // Return the decoded payload
     } catch (err) {
--- frontend/src/app/profile/page.tsx
+++ frontend/src/app/profile/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { useUser } from "@auth0/nextjs-auth0/client";
 import React from "react";
-<<<<<<< HEAD
 import { useGetTodosQuery } from "../../../graphql/dist/graphql";
 import { getAccessToken } from "@auth0/nextjs-auth0";
 import { error } from "console";
@@ -11,10 +10,6 @@ const Page = () => {
   console.log("result: ", result.data);
   console.log("error", result.error);
 
-=======
-
-const Page = () => {
->>>>>>> 30e85c345dc301052442a9b4e75b4990a2a1da14
   const { user } = useUser();
   return (
     <div>
